6a8a0c5efb628917a66426a053aad14a4559372d
Beno!t POLASZEK <bpolaszek@gmail.com>
2024-12-19 14:30:07 +0100

    feat: improve UI

--- examples/App.vue
+++ examples/App.vue
@@ -39,6 +39,9 @@
           <label class="block mb-2">Passphrase (optional)</label>
           <input v-model="passphrase" type="text" class="border p-2 rounded w-full" placeholder="Enter passphrase" />
         </div>
+
+        <section v-html="keyGenerationCodeBlock" class="*:rounded-lg *:py-2 *:px-4 *:leading-tight text-sm" />
+
         <button @click="generateKeyPair" class="bg-blue-500 text-white px-4 py-2 rounded">Generate Keys</button>
       </div>
 
@@ -62,6 +65,7 @@
           <div class="flex mb-2 items-center gap-3">
             <label>Public Key</label>
             <button
+              v-if="serializedKeys.publicKey"
               type="button"
               class="text-xs opacity-60 text-light"
               @click="encryptionPublicKey = serializedKeys.publicKey">
@@ -80,6 +84,9 @@
             class="border p-2 rounded w-full h-32"
             placeholder="Enter message to encrypt"></textarea>
         </div>
+
+        <section v-html="encryptionCodeBlock" class="*:rounded-lg *:py-2 *:px-4 *:leading-tight text-sm" />
+
         <button @click="encryptMessage" class="bg-green-500 text-white px-4 py-2 rounded">Encrypt</button>
       </div>
 
@@ -97,6 +104,7 @@
           <div class="flex mb-2 items-center gap-3">
             <label>Private Key</label>
             <button
+              v-if="serializedKeys.privateKey"
               type="button"
               class="text-xs opacity-60 text-light"
               @click="decryptionPrivateKey = serializedKeys.privateKey">
@@ -111,7 +119,11 @@
         <div>
           <div class="flex mb-2 items-center gap-3">
             <label>Passphrase (if required)</label>
-            <button type="button" class="text-xs opacity-60 text-light" @click="decryptionPassphrase = passphrase">
+            <button
+              v-if="passphrase"
+              type="button"
+              class="text-xs opacity-60 text-light"
+              @click="decryptionPassphrase = passphrase">
               Paste from above
             </button>
           </div>
@@ -124,7 +136,11 @@
         <div>
           <div class="flex mb-2 items-center gap-3">
             <label>Encrypted Secret</label>
-            <button type="button" class="text-xs opacity-60 text-light" @click="secretToDecrypt = serializedSecret">
+            <button
+              v-if="serializedSecret"
+              type="button"
+              class="text-xs opacity-60 text-light"
+              @click="secretToDecrypt = serializedSecret">
               Paste from above
             </button>
           </div>
@@ -133,6 +149,9 @@
             class="border p-2 rounded w-full h-32"
             placeholder="Paste encrypted secret here"></textarea>
         </div>
+
+        <section v-html="decryptionCodeBlock" class="*:rounded-lg *:py-2 *:px-4 *:leading-tight text-sm" />
+
         <button @click="decryptMessage" class="bg-purple-500 text-white px-4 py-2 rounded">Decrypt</button>
       </div>
 
@@ -145,9 +164,15 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import {reactive, ref} from 'vue'
 import {Secret, SerializedKeyPair} from '../src/common'
 import {decrypt, encrypt, exportKeyPair, generateKeyPair as doGenerateKeyPair} from '../src'
+import {codeToHtml} from 'shiki/bundle/web'
+import {asyncComputed} from '@vueuse/core'
+import match from 'match-operator'
+
+const code = 'const a = 1' // input code
+const SHIKI_PARAMS = {lang: 'javascript', theme: 'vitesse-dark'}
 
 // Key Generation
 const algorithm = ref<'RSA' | 'ECC' | 'AES'>('RSA')
@@ -156,6 +181,30 @@ const eccCurve = ref<'P-256' | 'P-384' | 'P-521'>('P-256')
 const passphrase = ref('')
 const keyPair = ref<CryptoKeyPair | null>(null)
 const serializedKeys = ref<SerializedKeyPair>({publicKey: '', privateKey: ''})
+const keyGenerationCodeBlock = asyncComputed(() => {
+  const params: any = match(algorithm.value, {
+    RSA: reactive({algorithm: 'RSA', rsaModulusLength}),
+    ECC: reactive({algorithm: 'ECC', eccCurve}),
+    AES: {algorithm: 'AES'},
+  })
+  if (passphrase.value.length > 0) {
+    params.passphrase = passphrase.value
+  }
+
+  const keyGenParamsAsString = JSON.stringify(params, null, 2).replace(/"([^"]+)":/g, '$1:')
+  return codeToHtml(
+    `
+import {generateKeyPair, exportKeyPair} from 'letsdecrypt'
+
+// Generate key pair
+const keyPair = await generateKeyPair(${keyGenParamsAsString})
+
+// Export keys
+const {publicKey, privateKey} = await exportKeyPair(keyPair)
+  `.trim(),
+    SHIKI_PARAMS
+  )
+})
 
 // Encryption
 const encryptionPublicKey = ref('')
@@ -163,6 +212,19 @@ const messageToEncrypt =
   ref(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus purus, ultricies eget urna eu, ultricies suscipit sapien. Aliquam molestie suscipit dolor, et egestas ex bibendum ac.
 Donec ac laoreet massa, ac lobortis ex. Fusce ac urna dolor. Etiam in consequat nibh. Vivamus ante tortor, congue ac diam in, viverra sollicitudin tortor.
 Fusce ipsum diam, molestie a cursus id, vehicula posuere ipsum. Proin in eros est. Vivamus tincidunt, leo eget placerat ultrices, ante erat pharetra lacus, eget sodales mi libero sed risus.`)
+
+const encryptionCodeBlock = asyncComputed(() => {
+  return codeToHtml(
+    `
+import {encrypt} from 'letsdecrypt'
+
+// Encrypt message
+const encryptedSecret = await encrypt(payload, publicKey)
+  `.trim(),
+    SHIKI_PARAMS
+  )
+})
+
 const encryptedSecret = ref<Secret | null>(null)
 const serializedSecret = ref('')
 
@@ -171,6 +233,30 @@ const decryptionPrivateKey = ref('')
 const decryptionPassphrase = ref('')
 const secretToDecrypt = ref('')
 const decryptedMessage = ref('')
+
+const decryptionCodeBlock = asyncComputed(() => {
+  if (decryptionPassphrase.value.length > 0) {
+    return codeToHtml(
+      `
+import {decrypt} from 'letsdecrypt'
+
+// Decrypt message
+const decryptedSecret = await decrypt(encryptedSecret, privateKey, "${decryptionPassphrase.value}")
+  `.trim(),
+      SHIKI_PARAMS
+    )
+  } else {
+    return codeToHtml(
+      `
+import {decrypt} from 'letsdecrypt'
+
+// Decrypt message
+const decryptedSecret = await decrypt(encryptedSecret, privateKey)
+  `.trim(),
+      SHIKI_PARAMS
+    )
+  }
+})
 
 async function generateKeyPair() {
   try {
